ebe83de4a14786df0c392ead9fbfd4ce2bc309f1
Stas Persaev <persaev10@mail.ru>
2026-07-06 13:18:46 +0300

    Добавил переход в полный экран при нажатии на F11

--- BjjScoreboard/MainWindow.xaml.cs
+++ BjjScoreboard/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
+using System.Windows.Media.Animation;
 
 namespace BjjScoreboard
 {
@@ -23,6 +24,46 @@ namespace BjjScoreboard
                 if (vm != null)
                 {
                     vm.TogglePauseCommand.Execute(null);
+                }
+                e.Handled = true;
+            }
+
+            // Переключение полноэкранного режима по нажатию F11
+            if (e.Key == Key.F11)
+            {
+                if (this.WindowStyle == WindowStyle.None)
+                {
+                    // Возвращаем в обычное оконное состояние
+                    this.WindowStyle = WindowStyle.SingleBorderWindow;
+                    this.WindowState = WindowState.Normal;
+                    this.ResizeMode = ResizeMode.CanResize;
+                }
+                else
+                {
+                    // Включаем чистый Fullscreen
+                    this.WindowStyle = WindowStyle.None;
+                    this.WindowState = WindowState.Maximized;
+                    this.ResizeMode = ResizeMode.NoResize;
+
+                    // --- ЛОГИКА ИСЧЕЗАЮЩЕГО УВЕДОМЛЕНИЯ ---
+                    // 1. Показываем уведомление
+                    FullscreenNotification.IsOpen = true;
+                    FullscreenNotification.Child.Opacity = 1.0;
+
+                    // 2. Создаем анимацию угасания (от 1.0 до 0.0 прозрачности)
+                    DoubleAnimation fadeAnimation = new DoubleAnimation
+                    {
+                        From = 1.0,
+                        To = 0.0,
+                        BeginTime = TimeSpan.FromSeconds(2.5), // Ждем 2.5 секунды перед началом исчезновения
+                        Duration = TimeSpan.FromSeconds(0.8)   // Само исчезновение длится 0.8 секунды
+                    };
+
+                    // 3. Когда анимация завершится, полностью закрываем Popup
+                    fadeAnimation.Completed += (s, args) => { FullscreenNotification.IsOpen = false; };
+
+                    // 4. Запускаем анимацию на внутреннем элементе Border
+                    FullscreenNotification.Child.BeginAnimation(UIElement.OpacityProperty, fadeAnimation);
                 }
                 e.Handled = true;
             }
